994d1bea50e958a8e0ea2184f5eedaaeaaa49cf7
+ <+>
2024-12-09 17:14:06 +0300

    DOM elements

---
+++ index.js
@@ -0,0 +1,7 @@
+ //Initializing an empty array
+ let shoppingList=[];
+
+ //DOM elements
+ const form=document.getElementById("shoppinglist-form");
+ const button=document.getElementById("actions");
+ const purchase=document.getElementById("markPurchased");
